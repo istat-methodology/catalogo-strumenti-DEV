452bf8d8b546958f47185e49e697410ac5cf9b9c
SMacOne <s.macone@libero.it>
2021-10-21 17:29:08 +0200

    fix

--- catalogo-strumenti-frontend/src/views/catalogue/tools/ToolDetails.vue
+++ catalogo-strumenti-frontend/src/views/catalogue/tools/ToolDetails.vue
@@ -36,7 +36,6 @@
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </CTab>
-          
           </CTabs>
         </CCardBody>
         <CCardFooter>
