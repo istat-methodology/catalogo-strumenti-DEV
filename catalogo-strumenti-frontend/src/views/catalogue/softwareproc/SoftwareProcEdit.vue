56efbe74c83f599674a593a71abf83d8a518eb2b
SMacOne <s.macone@libero.it>
2021-11-18 10:43:09 +0100

    S Proc SERVICE

---
+++ catalogo-strumenti-frontend/src/views/catalogue/softwareproc/SoftwareProcEdit.vue
@@ -0,0 +1,134 @@
+<template>
+  <!-- wait until service is loaded -->
+  <div class="row">
+    <div class="col-12">
+      <CCard>
+        <CCardHeader v-if="deskapp">Desktop Application</CCardHeader>
+        <CCardBody>
+          <CInput
+            label="Dipendenze"
+            placeholder="Dipendenze"
+            v-model="deskapp.dipendenze"
+          />
+          <CInput
+            label="Download"
+            placeholder="Download"
+            v-model="deskapp.download"
+          />
+          <CInput
+            label="Licenza"
+            placeholder="Liceenza"
+            v-model="deskapp.licenza"
+          />
+          <CInput
+            label="Linguaggio"
+            placeholder="Linguaggio"
+            v-model="deskapp.linguaggio"
+          />
+          <CInput
+            label="Pacchetto"
+            placeholder="Pacchetto"
+            v-model="deskapp.pacchetto"
+          />
+          <CInput
+            label="Sistema Operativo"
+            placeholder="Sistema Operativo"
+            v-model="deskapp.sistemaOperativo"
+          />
+          <label>Tool</label>
+          <v-select
+            label="nome"
+            :options="toolscatalog"
+            :reduce="option => option.id"
+            v-model="deskapp.tool"
+            placeholder="Tool"
+            :class="{
+              'is-invalid': $v.deskapp.tool.$error
+            }"
+          ></v-select>
+        </CCardBody>
+        <CCardFooter>
+          <CButton
+            shape="square"
+            size="sm"
+            color="primary"
+            class="mr-2"
+            @click.prevent="handleSubmit"
+            >Update</CButton
+          >
+          <CButton
+            shape="square"
+            size="sm"
+            color="light"
+            @click.prevent="backToList"
+            >Back</CButton
+          >
+        </CCardFooter>
+      </CCard>
+    </div>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import { required } from "vuelidate/lib/validators";
+/* import { required } from "vuelidate/lib/validators"; */
+export default {
+  name: "DeskAppEdit",
+  computed: {
+    ...mapGetters("tools", ["toolscatalog"]),
+    ...mapGetters("applications", ["application"])
+  },
+  data() {
+    return {
+      deskapp: {
+        id: "",
+        dipendenze: "",
+        download: "",
+        licenza: "",
+        linguaggio: "",
+        pacchetto: "",
+        sistemaOperativo: "",
+        tool: ""
+      }
+    };
+  },
+  validations: {
+    deskapp: {
+      tool: {
+        required
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      if (!this.$v.deskapp.tool.$invalid) {
+        this.$store.dispatch("applications/update", this.deskapp).then(() => {
+          this.backToList();
+        });
+      }
+    },
+    setOldValues() {
+      this.deskapp.id = this.application.id;
+      this.deskapp.dipendenze = this.application.dipendenze;
+      this.deskapp.download = this.application.download;
+      this.deskapp.licenza = this.application.licenza;
+      this.deskapp.linguaggio = this.application.linguaggio;
+      this.deskapp.pacchetto = this.application.pacchetto;
+      this.deskapp.sistemaOperativo = this.application.sistemaOperativo;
+      this.deskapp.tool = this.application.tool.id;
+    },
+
+    backToList() {
+      this.$router.push("/catalogue/deskapplist");
+    }
+  },
+  created() {
+    this.$store.dispatch("tools/findAll");
+    this.$store
+      .dispatch("applications/findById", this.$route.params.id)
+      .then(() => {
+        this.setOldValues();
+      });
+  }
+};
+</script>
---
+++ catalogo-strumenti-frontend/src/views/catalogue/softwareproc/SoftwareProcList.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="row">
+    <div class="col-12" v-if="isLoading">
+      <tile></tile>
+    </div>
+    <div class="col-12" v-else>
+      <div class="card fade-in">
+        <header class="card-header">
+          Desktop Application List
+          <div class="card-header-actions">
+            <router-link tag="a" :to="{ name: 'DeskAppAdd' }">
+              <add-icon />
+            </router-link>
+          </div>
+        </header>
+        <CCardBody>
+          <CDataTable
+            :items="applications"
+            :fields="fields"
+            :items-per-page="10"
+            sorter
+            hover
+            pagination
+          >
+            <template #show_update="{item}">
+              <td>
+                <router-link
+                  tag="a"
+                  :to="{
+                    name: 'DeskAppEdit',
+                    params: { id: item.id }
+                  }"
+                >
+                  <edit-icon />
+                </router-link>
+              </td>
+            </template>
+            <template #show_delete="{item}">
+              <td>
+                <span class="icon-link" @click="modalOpen(item)"
+                  ><delete-icon
+                /></span>
+              </td>
+            </template>
+          </CDataTable>
+        </CCardBody>
+      </div>
+    </div>
+    <CModal title="Warning!" :show.sync="warningModal">
+      <template #footer>
+        <CButton shape="square" size="sm" color="light" @click="modalClose">
+          Close
+        </CButton>
+        <CButton
+          shape="square"
+          size="sm"
+          color="primary"
+          @click="deleteDeskApp"
+        >
+          Delete
+        </CButton>
+      </template>
+      Delete Desktop Application '{{ selectedDeskApp.dipendenze }}'?
+    </CModal>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "DeskAppList",
+  data() {
+    return {
+      warningModal: false,
+      selectedDeskApp: {},
+      fields: [
+        /*  {
+          key: "id",
+          label: "Identificativo",
+          _style: "width:10%;"
+        }, */
+        {
+          key: "dipendenze",
+          label: "Dipendenze",
+          _style: "width:10%;"
+        },
+        {
+          key: "download",
+          label: "Download",
+          _style: "width:10%;"
+        },
+        {
+          key: "licenza",
+          label: "Licenza",
+          _style: "width:10%;"
+        },
+        {
+          key: "linguaggio",
+          label: "Linguaggio",
+          _style: "width:10%;"
+        },
+        {
+          key: "pacchetto",
+          label: "Pacchetto",
+          _style: "width:10%;"
+        },
+        {
+          key: "sistemaOperativo",
+          label: "Sistema Operativo",
+          _style: "width:10%;"
+        },
+        {
+          key: "tool",
+          label: "Tool",
+          _style: "width:10%;"
+        },
+        {
+          key: "show_update",
+          label: "",
+          _style: "width:1%",
+          sorter: false,
+          filter: false
+        },
+        {
+          key: "show_delete",
+          label: "",
+          _style: "width:1%",
+          sorter: false,
+          filter: false
+        }
+      ]
+    };
+  },
+  computed: {
+    ...mapGetters("coreui", ["isLoading"]),
+    ...mapGetters("applications", ["applications"]),
+    nomeTool() {
+      return this.applications.map(
+        ...item => {
+          item, { unit: item.tool.nome };
+        }
+      );
+    }
+  },
+  methods: {
+    deleteDeskApp() {
+      this.$store.dispatch("applications/delete", this.selectedDeskApp.id);
+      this.warningModal = false;
+    },
+    modalOpen(app) {
+      this.selectedDeskApp = app;
+      this.warningModal = true;
+    },
+    modalClose() {
+      this.warningModal = false;
+    }
+  },
+  created() {
+    this.$store.dispatch("applications/findAll");
+  }
+};
+</script>
