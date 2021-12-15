a165e381260172672795ea90fc681807028739ab
renzo80 <iannacone@istat.it>
2021-12-15 11:58:12 +0100

    Updated catalog database

--- catalogo-strumenti-backend/catalogo-backend-service/db/catalog-create-mysql.sql
+++ catalogo-strumenti-backend/catalogo-backend-service/db/catalog-create-mysql.sql
@@ -6,60 +6,67 @@ CREATE SCHEMA `catalog` DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 use catalog;
 
  
--- Tabellina di appoggio. Il db dovr� collegarsi a quella di IS2.
- CREATE TABLE IF NOT EXISTS `catalog`.`business_function` (
+-- Tabellina di appoggio. Il db dovrà collegarsi a quella relativa di IS2.
+DROP TABLE IF EXISTS `catalog`.`is2_business_service` ;
+
+ CREATE TABLE IF NOT EXISTS `catalog`.`is2_business_service` (
   `ID` INT(11) NOT NULL AUTO_INCREMENT,
   `NAME` VARCHAR(100) NULL DEFAULT NULL,
-  `DESCR` TEXT NULL DEFAULT NULL,
-  `LABEL` VARCHAR(50) NULL DEFAULT NULL,
-  `ACTIVE` INT(11) NULL DEFAULT NULL,
-  `gsbpm` INT NULL,
+  `DESCR` TEXT NULL DEFAULT NULL,  
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 201
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
--- -----------------------------------------------------
--- Table `catalog`.`CSM_Methodological_Tool`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Methodological_Tool` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `Data` VARCHAR(100) NULL DEFAULT NULL,
-  `Descrizione` VARCHAR(1000) NULL DEFAULT NULL,
-  `Metodo_Statistico` VARCHAR(100) NULL DEFAULT NULL,  
-  `Nome` VARCHAR(100) NULL DEFAULT NULL,
-  `Note_di_compilazione` VARCHAR(100) NULL DEFAULT NULL,
-  `Standard` VARCHAR(100) NULL DEFAULT NULL,
-  `Tags` VARCHAR(500) NULL DEFAULT NULL,
-  `Versione` VARCHAR(100) NULL DEFAULT NULL,
-  `Tipologia` VARCHAR(100) NULL DEFAULT NULL,
-  `Riferimenti` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`));
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Software_Procedure`
+-- Tabellina di appoggio. Il db dovrà collegarsi a quella relativa di IS2.
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Software_Procedure` (
+DROP TABLE IF EXISTS `catalog`.`is2_app_service` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`is2_app_service` (
   `ID` INT NOT NULL AUTO_INCREMENT,
-  `Codice` VARCHAR(100) NULL DEFAULT NULL,
-  `Sintassi` VARCHAR(100) NULL DEFAULT NULL,
-  `Dipendenze` VARCHAR(100) NULL DEFAULT NULL,
-  `Linguaggio` VARCHAR(100) NULL DEFAULT NULL,
-  `tool` INT NULL DEFAULT NULL,
+  `NAME` VARCHAR(100) NULL DEFAULT NULL,
+  `DESCR` TEXT NULL DEFAULT NULL,
+  `IMPLEMENTATION_LANGUAGE` VARCHAR(100) NULL DEFAULT NULL,
+  `ENGINE` VARCHAR(100) NULL DEFAULT NULL,
+  `SOURCE_PATH` VARCHAR(100) NULL DEFAULT NULL,
+  `SOURCE_CODE` LONGTEXT NULL DEFAULT NULL,
+  `AUTHOR` VARCHAR(100) NULL DEFAULT NULL,
+  `LICENCE` VARCHAR(100) NULL DEFAULT NULL,
+  `CONTACT` VARCHAR(100) NULL DEFAULT NULL,
+  `BUSINESS_SERVICE_ID` INT NOT NULL,
   PRIMARY KEY (`ID`),
-  INDEX `fk_CSM_Software_Procedure_is2_Methodological_Tool` (`tool` ASC) VISIBLE,
-  CONSTRAINT `fk_CSM_Software_Procedure_is2_Methodological_Tool`
-    FOREIGN KEY (`tool`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  CONSTRAINT `fk_is2_app_service_is2_business_service`
+    FOREIGN KEY (`BUSINESS_SERVICE_ID`)
+    REFERENCES `catalog`.`is2_business_service` (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 251
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_is2_app_service_is2_business_service` ON `catalog`.`is2_app_service` (`BUSINESS_SERVICE_ID` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Statistical_Method`
+-- Table `catalog`.`cls_tool_type`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Statistical_Method` (
+DROP TABLE IF EXISTS `catalog`.`cls_tool_type` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`cls_tool_type` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `catalog`.`csm_statistical_method`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `catalog`.`csm_statistical_method` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_statistical_method` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Nome` VARCHAR(100) NULL DEFAULT NULL,
   `Autore` VARCHAR(100) NULL DEFAULT NULL,
@@ -69,38 +76,124 @@ CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Statistical_Method` (
   `Ipotesi` VARCHAR(100) NULL DEFAULT NULL,
   `Limiti` VARCHAR(100) NULL DEFAULT NULL,
   `Indicatori_qualita` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `catalog`.`csm_methodological_tool`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `catalog`.`csm_methodological_tool` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_methodological_tool` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Data` VARCHAR(100) NULL DEFAULT NULL,
+  `Descrizione` VARCHAR(1000) NULL DEFAULT NULL,
+  `homepage` VARCHAR(100) NULL DEFAULT NULL,
+  `download` VARCHAR(100) NULL DEFAULT NULL,
+  `Metodo_Statistico` VARCHAR(100) NULL DEFAULT NULL,
+  `Name` VARCHAR(100) NULL DEFAULT NULL,
+  `Nome` VARCHAR(100) NULL DEFAULT NULL,
+  `Note` VARCHAR(100) NULL DEFAULT NULL,
+  `Standard` VARCHAR(100) NULL DEFAULT NULL,
+  `Tags` VARCHAR(500) NULL DEFAULT NULL,
+  `Versione` VARCHAR(100) NULL DEFAULT NULL,
+  `Tipologia` VARCHAR(100) NULL DEFAULT NULL,
+  `Riferimenti` VARCHAR(100) NULL DEFAULT NULL,
+  `Licence` VARCHAR(45) NULL DEFAULT NULL,
+  `is2_business_service_ID` INT NOT NULL,
+  `csm_tool_type_id` INT NOT NULL,
+  `csm_statistical_method_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `is2_business_service_ID`, `csm_statistical_method_ID`),
+  CONSTRAINT `fk_csm_methodological_tool_is2_business_service1`
+    FOREIGN KEY (`is2_business_service_ID`)
+    REFERENCES `catalog`.`is2_business_service` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_csm_methodological_tool_csm_tool_type1`
+    FOREIGN KEY (`csm_tool_type_id`)
+    REFERENCES `catalog`.`cls_tool_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_csm_methodological_tool_csm_statistical_method1`
+    FOREIGN KEY (`csm_statistical_method_ID`)
+    REFERENCES `catalog`.`csm_statistical_method` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 201
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_csm_methodological_tool_is2_business_service1_idx` ON `catalog`.`csm_methodological_tool` (`is2_business_service_ID` ASC) VISIBLE;
+
+CREATE INDEX `fk_csm_methodological_tool_csm_tool_type1_idx` ON `catalog`.`csm_methodological_tool` (`csm_tool_type_id` ASC) VISIBLE;
+
+CREATE INDEX `fk_csm_methodological_tool_csm_statistical_method1_idx` ON `catalog`.`csm_methodological_tool` (`csm_statistical_method_ID` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `catalog`.`csm_software_procedure`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `catalog`.`csm_software_procedure` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_software_procedure` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Codice` VARCHAR(100) NULL DEFAULT NULL,
+  `Sintassi` VARCHAR(100) NULL DEFAULT NULL,
+  `Dipendenze` VARCHAR(100) NULL DEFAULT NULL,
+  `Linguaggio` VARCHAR(100) NULL DEFAULT NULL,
   `tool` INT NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  INDEX `fk_Statistical_Method_is2_Methodological_Tool` (`tool` ASC) VISIBLE,
-  CONSTRAINT `fk_Statistical_Method_is2_Methodological_Tool`
+  CONSTRAINT `fk_CSM_Software_Procedure_is2_Methodological_Tool`
     FOREIGN KEY (`tool`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `catalog`.`csm_methodological_tool` (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 101
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_CSM_Software_Procedure_is2_Methodological_Tool` ON `catalog`.`csm_software_procedure` (`tool` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Statistical_Service`
+-- Table `catalog`.`csm_statistical_service`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Statistical_Service` (
+DROP TABLE IF EXISTS `catalog`.`csm_statistical_service` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_statistical_service` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Metodi_esposti` VARCHAR(100) NULL DEFAULT NULL,
   `Dipendenze` VARCHAR(100) NULL DEFAULT NULL,
   `Protocollo` VARCHAR(100) NULL DEFAULT NULL,
   `tool` INT NULL DEFAULT NULL,
+  `is2_app_service_ID` INT NOT NULL,
   PRIMARY KEY (`ID`),
-  INDEX `fk_CSM_Statistical_Service_is2_Methodological_Tool` (`tool` ASC) VISIBLE,
-  CONSTRAINT `fk_CSM_Statistical_Service_is2_Methodological_Tool`
+  CONSTRAINT `fk_CSM_Statistical_Service_csm_Methodological_Tool`
     FOREIGN KEY (`tool`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
+    REFERENCES `catalog`.`csm_methodological_tool` (`ID`),
+  CONSTRAINT `fk_csm_statistical_service_is2_app_service1`
+    FOREIGN KEY (`is2_app_service_ID`)
+    REFERENCES `catalog`.`is2_app_service` (`ID`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_CSM_Statistical_Service_csm_Methodological_Tool` ON `catalog`.`csm_statistical_service` (`tool` ASC) VISIBLE;
+
+CREATE INDEX `fk_csm_statistical_service_is2_app_service1_idx` ON `catalog`.`csm_statistical_service` (`is2_app_service_ID` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Desktop_Application`
+-- Table `catalog`.`csm_desktop_application`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Desktop_Application` (
+DROP TABLE IF EXISTS `catalog`.`csm_desktop_application` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_desktop_application` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Dipendenze` VARCHAR(100) NULL DEFAULT NULL,
   `Download` VARCHAR(100) NULL DEFAULT NULL,
@@ -110,80 +203,80 @@ CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Desktop_Application` (
   `Sistema_Operativo` VARCHAR(100) NULL DEFAULT NULL,
   `tool` INT NULL DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  INDEX `fk_CSM_Desktop_Application_is2_Methodological_Tool` (`tool` ASC) VISIBLE,
-  CONSTRAINT `fk_CSM_Desktop_Application_is2_Methodological_Tool`
+  CONSTRAINT `fk_CSM_Desktop_Application_csm_Methodological_Tool`
     FOREIGN KEY (`tool`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `catalog`.`csm_methodological_tool` (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_CSM_Desktop_Application_csm_Methodological_Tool` ON `catalog`.`csm_desktop_application` (`tool` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Agent`
+-- Table `catalog`.`csm_agent`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Agent` (
+DROP TABLE IF EXISTS `catalog`.`csm_agent` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_agent` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Nome` VARCHAR(100) NULL DEFAULT NULL,
   `Organizzazione` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`));
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 31
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_link_Agent_tool`
+-- Table `catalog`.`csm_link_agent_tool`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_link_Agent_tool` (
+DROP TABLE IF EXISTS `catalog`.`csm_link_agent_tool` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_link_agent_tool` (
   `agent` INT NOT NULL,
   `tool` INT NOT NULL,
   `Ruolo` VARCHAR(100) NULL DEFAULT NULL,
   `Note` VARCHAR(100) NULL DEFAULT NULL,
   `Date_riferimento` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`agent`, `tool`),
-  INDEX `fk_CSM_link_Agent_tool_CSM_Methodological_Tool` (`tool` ASC) VISIBLE,
   CONSTRAINT `fk_CSM_link_Agent_CSM_Methodological_tool`
     FOREIGN KEY (`agent`)
-    REFERENCES `catalog`.`CSM_Agent` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `catalog`.`csm_agent` (`ID`),
   CONSTRAINT `fk_CSM_link_Agent_tool_CSM_Methodological_Tool`
     FOREIGN KEY (`tool`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES `catalog`.`csm_methodological_tool` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE INDEX `fk_CSM_link_Agent_tool_CSM_Methodological_Tool` ON `catalog`.`csm_link_agent_tool` (`tool` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `catalog`.`CSM_Documentation`
+-- Table `catalog`.`csm_documentation`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Documentation` (
+DROP TABLE IF EXISTS `catalog`.`csm_documentation` ;
+
+CREATE TABLE IF NOT EXISTS `catalog`.`csm_documentation` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `Nome` VARCHAR(100) NULL DEFAULT NULL,
-  `Organizzazione` VARCHAR(100) NULL DEFAULT NULL,
-  `CSM_Methodological_Tool_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`, `CSM_Methodological_Tool_ID`),
-  INDEX `fk_CSM_Documentation_CSM_Methodological_Tool1_idx` (`CSM_Methodological_Tool_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_CSM_Documentation_CSM_Methodological_Tool1`
-    FOREIGN KEY (`CSM_Methodological_Tool_ID`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
+  `EDITORE` VARCHAR(100) NULL DEFAULT NULL,
+  `Type` VARCHAR(100) NULL DEFAULT NULL,
+  `Note` VARCHAR(100) NULL DEFAULT NULL,
+  `Resource` VARCHAR(1000) NULL DEFAULT NULL,
+  `tool` INT NOT NULL,
+  PRIMARY KEY (`ID`, `tool`),
+  CONSTRAINT `fk_csm_documentation_csm_methodological_tool1`
+    FOREIGN KEY (`tool`)
+    REFERENCES `catalog`.`csm_methodological_tool` (`ID`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
-
--- -----------------------------------------------------
--- Table `catalog`.`CSM_Methodological_Tool_has_is2_business_function`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `catalog`.`CSM_Methodological_Tool_has_is2_business_function` (
-  `CSM_Methodological_Tool_ID` INT NOT NULL,
-  `is2_business_function_ID` INT(11) NOT NULL,
-  PRIMARY KEY (`CSM_Methodological_Tool_ID`, `is2_business_function_ID`),
-  INDEX `fk_CSM_Methodological_Tool_has_is2_business_function_is2_bu_idx` (`is2_business_function_ID` ASC) VISIBLE,
-  INDEX `fk_CSM_Methodological_Tool_has_is2_business_function_CSM_Me_idx` (`CSM_Methodological_Tool_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_CSM_Methodological_Tool_has_is2_business_function_CSM_Meth1`
-    FOREIGN KEY (`CSM_Methodological_Tool_ID`)
-    REFERENCES `catalog`.`CSM_Methodological_Tool` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_CSM_Methodological_Tool_has_is2_business_function_is2_busi1`
-    FOREIGN KEY (`is2_business_function_ID`)
-    REFERENCES `catalog`.`business_function` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE INDEX `fk_csm_documentation_csm_methodological_tool1_idx` ON `catalog`.`csm_documentation` (`tool` ASC) VISIBLE;
--- catalogo-strumenti-backend/catalogo-backend-service/db/catalog-init.sql
+++ catalogo-strumenti-backend/catalogo-backend-service/db/catalog-init.sql
@@ -1,52 +1,65 @@
 use catalog;
 
-INSERT INTO CSM_Methodological_Tool	(`ID`,`Data`,`Descrizione`,`Metodo_Statistico`,`Nome`,`Note_di_compilazione`, `Standard`, `Tags`, `Versione`, `Tipologia`, `Riferimenti`)
-values (100, 'Today','Detection of outliers and influential errors using a latent variable model',
-'Selective Editing via Mixture Models','Selemix','Note',	'ISTAT',
-'mixture, selective editing, outliers',	'1.0.2','package','https://rdrr.io/cran/SeleMix/');
-
 -- Dati di test
-INSERT INTO business_function (ID, NAME, DESCR, LABEL, ACTIVE, GSBPM) 
+INSERT INTO is2_business_service (ID, NAME, DESCR) 
 	VALUES 
-		(80,'Specify needs',NULL,NULL,1,NULL),
-		(110,'Design',NULL,NULL,2,NULL),
-		(130,'Build',NULL,NULL,3,NULL);
+		(91,'ARC','ARC file loader INSEE'),	
+		(100,'Selemix','Selective Editing via Mixture Models'),			
+		(200,'Relais','Record Linkage at Istat');
 		
-INSERT INTO CSM_Methodological_Tool	(`ID`, `Data`, `Descrizione`, `Metodo_Statistico`, `Nome`, `Note_di_compilazione`, `Standard`, `Tags`, `Versione`, `Tipologia`, `Riferimenti`)
+		
+-- Dati di test
+INSERT INTO cls_tool_type (ID, NAME, DESCRIPTION) 
+	VALUES 
+		(1,'ARC','ARC file loader INSEE'),		
+		(2,'Relais','Record Linkage at Istat'),
+		(3,'Selemix','Selective Editing via Mixture Models');	
+		
+INSERT INTO `catalog`.`csm_statistical_method` (`ID`, `Nome`, `Autore`, `Obiettivo`, `Descrizione`, `Generalita`, `Ipotesi`, `Limiti`, `Indicatori_qualita`) VALUES ('1', 'Metodo di test', 'Antonio Bianchi', 'Test', 'Effettua una test', 'Prova', 'Ciao', 'Alcuni', 'Vari');
+INSERT INTO `catalog`.`csm_statistical_method` (`ID`, `Nome`, `Autore`, `Obiettivo`, `Descrizione`, `Generalita`, `Ipotesi`, `Limiti`, `Indicatori_qualita`) VALUES ('2', 'Metodo di test n2', 'Franco Rossi', 'Test2', 'Fa una prova', 'Prova2', 'CiaoCiao', 'Alcuni', 'Vari');
+		
+		
+INSERT INTO CSM_Methodological_Tool	(`ID`,`Data`,`Descrizione`, `homepage`,`download`,`Metodo_Statistico`, `Name`,`Nome`,`Note`,`Standard`, `Tags`,`Versione`,	`Tipologia`, `Riferimenti`,`Licence`,  `is2_business_service_ID`, `csm_tool_type_id`, `csm_statistical_method_ID`)
+values (100, 'Today','Detection of outliers and influential errors using a latent variable model', 'www.istat.it','www.istat.it/download/Selemix',
+'Selective Editing via Mixture Models','Selemix', 'Selemix','Note',	'ISTAT',
+'mixture, selective editing, outliers',	'1.0.2','package','https://rdrr.io/cran/SeleMix/','l-gpl',100,3,2);
+
+INSERT INTO CSM_Methodological_Tool	(`ID`,`Data`,`Descrizione`, `homepage`,`download`,`Metodo_Statistico`, `Name`,`Nome`,`Note`,`Standard`, `Tags`,`Versione`,	`Tipologia`, `Riferimenti`,`Licence`,  `is2_business_service_ID`, `csm_tool_type_id`, `csm_statistical_method_ID`)
 values (200, 'Tomorrow','RELAIS (REcord Linkage At IStat) is a toolkit providing a set of techniques for dealing with record linkage projects.',
-'Record Linkage','Relais','Note','ISTAT','data integration, probabilistic record linkage, string comparators, blocking/sorting/indexing, 
-deduplication, open source software',	'3.1','desktop application','Available on registration');
+'www.istat.it','www.istat.it/download/Relais',
+'Record Linkage','Relais','Relais','Note','ISTAT','data integration, probabilistic record linkage, string comparators, blocking/sorting/indexing, 
+deduplication, open source software',	'3.1','desktop application','Available on registration','l-gpl',200,2,1);
 
 
+		
+INSERT INTO is2_app_service (ID, NAME, DESCR, IMPLEMENTATION_LANGUAGE, ENGINE, SOURCE_PATH, SOURCE_CODE, AUTHOR, LICENCE, CONTACT, BUSINESS_SERVICE_ID) 
+	VALUES 
+		(91, 'ARC LOADER', 'Java package implementing ARC loader service', 'JAVA', 'JAVA', 'it.istat.is2.catalogue.arc.service.LoadService', '', '', '', '', 91),	
+		(200, 'Relais', 'R package implementing record linkage methods', 'R', 'RENJIN', 'relais/relais.R', '', 'Istat', 'EUPL1.1', 'Luca Valentino (luvalent@istat.it)', 200);
 
- -- Link verso il process design esistente 
-INSERT INTO CSM_Methodological_Tool_has_is2_business_function (`CSM_Methodological_Tool_ID`,`is2_business_function_ID`) VALUES (100, 80);
-INSERT INTO CSM_Methodological_Tool_has_is2_business_function (`CSM_Methodological_Tool_ID`,`is2_business_function_ID`) VALUES (100, 110);
-INSERT INTO CSM_Methodological_Tool_has_is2_business_function (`CSM_Methodological_Tool_ID`,`is2_business_function_ID`) VALUES (100, 130);
 
 -- Specializzazioni del software
-INSERT INTO CSM_Software_Procedure (`Codice`,`Sintassi`,`Dipendenze`,`Linguaggio`, `tool` ) VALUES ('', '', 'mvtnorm, graphics', 'R',  100);
--- INSERT INTO CSM_Statistical_Method (`ID`,`Nome`,`Autore`,`Obiettivo`,`Descrizione`,`Generalità`,`Ipotesi`,`Limiti`,`Indicatori_qualità`,`tool`) VALUES (100);
+INSERT INTO CSM_Software_Procedure (`Codice`,`Sintassi`,`Dipendenze`,`Linguaggio`, `tool` ) VALUES ('300', 'test', 'mvtnorm, graphics', 'R',  100);
+
 INSERT INTO CSM_Desktop_Application (`Dipendenze`,`Download`,`Licenza`,`Linguaggio`,`Package`,`Sistema_Operativo`,`tool`) 
 VALUES('Java, R, MySQL',null,'Open Source','R, SQL','Desktop Application','Windows',200);
--- INSERT INTO CSM_Statistical_Service (`ID`,`Metodi_esposti`,`Dipendenze`,`Protocollo`,`tool`) VALUES();
+
 
 -- Sezione documentale esterna, link alle persone coinvolte nello sviluppo o nella documentazione
 INSERT INTO CSM_Agent (`ID`,`Nome`,`Organizzazione`) VALUES (10,'Mario Rossi','Istat');
 INSERT INTO CSM_Agent (`ID`,`Nome`,`Organizzazione`) VALUES (20,'Luca Verdi','Eurostat');
+INSERT INTO CSM_Agent (`ID`,`Nome`,`Organizzazione`) VALUES (30,'Luca Valente','Istat');
+
 INSERT INTO CSM_link_Agent_tool (`agent`,`tool`,`Ruolo`,`Note`,`Date_riferimento`) VALUES (10,100,'Sviluppatore','Note','2010-2020');
 INSERT INTO CSM_link_Agent_tool (`agent`,`tool`,`Ruolo`,`Note`,`Date_riferimento`) VALUES (20,100,'Referente','Note','2020 in poi');
-
-INSERT INTO CSM_Agent (`ID`,`Nome`,`Organizzazione`) VALUES (30,'Luca Valente','Istat');
 INSERT INTO CSM_link_Agent_tool (`agent`,`tool`,`Ruolo`,`Note`,`Date_riferimento`) VALUES (30,200,'Sviluppatore','Note','2005-2015');
 
     
 -- Sezione documentale esterna, link alle risorse di documentazione
-INSERT INTO CSM_Documentation (`ID`,`Nome`,`Organizzazione`,`CSM_Methodological_Tool_ID`) VALUES (10,'Selemix','Istat',100);
-INSERT INTO CSM_Documentation (`ID`,`Nome`,`Organizzazione`,`CSM_Methodological_Tool_ID`) VALUES (20,'Relais','Istat',200);
+INSERT INTO CSM_Documentation (`ID`,`Nome`,`Editore`,`Note`,`Resource`,`Type`,`tool`) VALUES (10,'Selemix','Istat','Selective editing','www.istat.it/resource/selemix','test',100);
+INSERT INTO CSM_Documentation (`ID`,`Nome`,`Editore`,`Note`,`Resource`,`Type`,`tool`) VALUES (20,'Relais','Istat','Probabilistic record linkage','www.istat.it/resource/relais','test',200);
 
-INSERT INTO `catalog`.`csm_statistical_method` (`ID`, `Nome`, `Autore`, `Obiettivo`, `Descrizione`, `Generalita`, `Ipotesi`, `Limiti`, `Indicatori_qualita`, `tool`) VALUES ('1', 'Metodo di test', 'Antonio Bianchi', 'Test', 'Effettua una test', 'Prova', 'Ciao', 'Alcuni', 'Vari', '100');
-INSERT INTO `catalog`.`csm_statistical_method` (`ID`, `Nome`, `Autore`, `Obiettivo`, `Descrizione`, `Generalita`, `Ipotesi`, `Limiti`, `Indicatori_qualita`, `tool`) VALUES ('2', 'Metodo di test n2', 'Franco Rossi', 'Test2', 'Fa una prova', 'Prova2', 'CiaoCiao', 'Alcuni', 'Vari', '200');
+
 	
-INSERT INTO `catalog`.`csm_statistical_service` (`ID`, `Metodi_esposti`, `Dipendenze`, `Protocollo`, `tool`) VALUES ('1', 'Vari', 'Nessuna', 'Http', '100');
-INSERT INTO `catalog`.`csm_statistical_service` (`ID`, `Metodi_esposti`, `Dipendenze`, `Protocollo`, `tool`) VALUES ('2', 'Alcuni', 'Varie', 'Ftp', '200');
+INSERT INTO `catalog`.`csm_statistical_service` (`ID`, `Metodi_esposti`, `Dipendenze`, `Protocollo`, `tool`, `is2_app_service_ID`) VALUES ('1', 'Vari', 'Nessuna', 'Http', '100', '91');
+INSERT INTO `catalog`.`csm_statistical_service` (`ID`, `Metodi_esposti`, `Dipendenze`, `Protocollo`, `tool`, `is2_app_service_ID`) VALUES ('2', 'Alcuni', 'Varie', 'Ftp', '200', '200');
