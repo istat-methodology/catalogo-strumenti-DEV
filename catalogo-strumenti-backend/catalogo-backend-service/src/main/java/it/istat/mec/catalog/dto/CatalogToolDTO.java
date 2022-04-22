113ec31c5ad12b0c2c88e4812d816a2c7e8fc29e
renzo80 <sigma80@gmail.com>
2022-04-22 14:45:24 +0200

    Added @DateTimeFormat annotation to tool and StatisticalMethod dtos

--- catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/dto/CatalogToolDTO.java
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/dto/CatalogToolDTO.java
@@ -1,9 +1,8 @@
 package it.istat.mec.catalog.dto;
-
-
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +14,7 @@ public class CatalogToolDTO implements Serializable {
 	private static final long serialVersionUID = -6905546979001300372L;
 	
 	private Integer id;	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")	
 	private Date releaseDate;	
 	private String description;		
 	private String name;	
@@ -22,12 +22,12 @@ public class CatalogToolDTO implements Serializable {
 	private String tags;	
 	private String version;
 	private ToolTypeDto toolType;	
-	private Date lastUpdate;	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")	
+	private String lastUpdate;
 	private String requirements;
 	private BusinessServiceMiniDto businessService;
 	private List<GSBPMProcessMiniDto> gsbpmProcesses;
 	private List<StatisticalMethodDto> statisticalMethods;
-	private List<LinkAgentToolDto> linkAgentsTools;
-	
+	private List<LinkAgentToolDto> linkAgentsTools;	
 	private List<DocumentationDto> documentations;
 }
--- catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/dto/StatisticalMethodDto.java
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/dto/StatisticalMethodDto.java
@@ -1,25 +1,30 @@
 package it.istat.mec.catalog.dto;
 import java.io.Serializable;
-import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+@EqualsAndHashCode(callSuper=false)
 @Data
 @NoArgsConstructor
 public class StatisticalMethodDto extends BaseDto  implements Serializable {
 	
 	private static final long serialVersionUID = -6819215887293605229L;
 	
-
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private String releaseDate; 
 	private String description;
 	private String requirements;	
 	private String assumptions;			
 	private String constraints;	
-	private String notes;		
-	private Date lastUpdate;		
+	private String notes;	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")	
+	private String lastUpdate;		
 	private String tags;	
 	private String version;	
-	private Date releaseDate; 
 	private String standardIstat;
+	
+	
 }
