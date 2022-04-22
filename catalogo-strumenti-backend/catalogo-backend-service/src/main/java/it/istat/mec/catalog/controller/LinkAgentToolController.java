165dc69fe38e90d8c0e91ad3f530eb4b1d93dbcb
renzo80 <sigma80@gmail.com>
2022-04-22 16:17:33 +0200

    Fixed some class annotations

--- catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/controller/LinkAgentToolController.java
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/controller/LinkAgentToolController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import it.istat.mec.catalog.dto.LinkAgentToolDto;
 import it.istat.mec.catalog.request.CreateLinkAgentToolRequest;
 import it.istat.mec.catalog.service.LinkAgentToolService;
-
+@RestController
+@RequestMapping("/catalog")
 public class LinkAgentToolController {
 
 	@Autowired
