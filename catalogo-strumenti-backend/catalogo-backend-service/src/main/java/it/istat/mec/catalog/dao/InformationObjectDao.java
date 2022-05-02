c9c27cc441a1cd993cbdcc4c8b676abc234128ae
renzo80 <iannacone@istat.it>
2022-05-02 13:16:42 +0200

    Added InformationObject and ProcessDesign daos

---
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/dao/InformationObjectDao.java
@@ -0,0 +1,18 @@
+package it.istat.mec.catalog.dao;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import it.istat.mec.catalog.domain.BusinessService;
+import it.istat.mec.catalog.domain.InformationObject;
+
+
+@Repository
+public interface InformationObjectDao extends JpaRepository<InformationObject, Integer> {
+	
+	List<InformationObject> findByBusinessService(BusinessService businessService);
+
+	public void save(Optional<InformationObject> informationObject);
+
+	public void delete(InformationObject informationObject);
+}
