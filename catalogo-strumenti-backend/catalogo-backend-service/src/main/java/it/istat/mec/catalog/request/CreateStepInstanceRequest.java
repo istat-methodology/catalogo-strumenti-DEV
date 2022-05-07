6646e82610f289a501720e323d4cb3a3f051f264
francescoamato <francamato@gmail.com>
2022-05-07 15:35:34 +0200

    fix stepinstance CRUD

--- catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/request/CreateStepInstanceRequest.java
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/request/CreateStepInstanceRequest.java
@@ -1,4 +1,5 @@
 package it.istat.mec.catalog.request;
+
 import java.io.Serializable;
 
 import lombok.Data;
@@ -6,12 +7,18 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class CreateStepInstanceRequest implements Serializable  {
+public class CreateStepInstanceRequest implements Serializable {
 
-	
 	private static final long serialVersionUID = -5844676942420048218L;
-	private Integer id;	
-	private String name;	
-	private String desc;
+	private Integer id;
+
+	private String descr;
+	private String method;
+
+	private Integer statMethod;
+
+	private String functionality;
+
+	private Integer appService;
 
 }
--- catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/service/StepInstanceService.java
+++ catalogo-strumenti-backend/catalogo-backend-service/src/main/java/it/istat/mec/catalog/service/StepInstanceService.java
@@ -1,7 +1,12 @@
 package it.istat.mec.catalog.service;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.istat.mec.catalog.dao.AppServiceDao;
+import it.istat.mec.catalog.dao.StatisticalMethodDao;
 import it.istat.mec.catalog.dao.StepInstanceDao;
 import it.istat.mec.catalog.domain.AppService;
 import it.istat.mec.catalog.domain.StepInstance;
@@ -16,51 +21,72 @@ public class StepInstanceService {
 
 	@Autowired
 	StepInstanceDao stepInstanceDao;
+	@Autowired
+	AppServiceDao appServiceDao;
+	@Autowired
+	StatisticalMethodDao statisticalMethodDao;
 
 	public List<StepInstanceDto> findAllStepInstances() {
-		
+
 		return Translators.translateSI(stepInstanceDao.findAll());
 
 	}
-	
+
 	public StepInstanceDto newStepInstance(CreateStepInstanceRequest request) {
 		StepInstance si = new StepInstance();
-		si = Translators.translate(request);		
+		si = Translators.translate(request);
+
+		if (!appServiceDao.findById(request.getAppService()).isPresent())
+			throw new NoDataException("Implementation  not present");
+		si.setAppService(appServiceDao.findById(request.getAppService()).get());
+
+		if (!statisticalMethodDao.findById(request.getStatMethod()).isPresent())
+			throw new NoDataException("Statistical method  not present");
+		si.setStatMethod(statisticalMethodDao.findById(request.getStatMethod()).get());
+
 		stepInstanceDao.save(si);
 		return Translators.translate(si);
 	}
-	
+
 	public StepInstanceDto findStepInstanceById(Integer id) {
 
 		if (!stepInstanceDao.findById(id).isPresent())
 			throw new NoDataException("StepInstance not present");
 		return Translators.translate(stepInstanceDao.findById(id).get());
 	}
-	
-	public StepInstanceDto updateStepInstance(CreateStepInstanceRequest request) {		
-		
+
+	public StepInstanceDto updateStepInstance(CreateStepInstanceRequest request) {
+
 		if (!stepInstanceDao.findById(request.getId()).isPresent())
 			throw new NoDataException("StepInstance not present");
-		
-		StepInstance si = stepInstanceDao.findById(request.getId()).get();	
-		
+
+		StepInstance si = stepInstanceDao.findById(request.getId()).get();
+
 		si = Translators.translateUpdate(request, si);
-		
-		stepInstanceDao.save(si);		
-		
+
+		if (!appServiceDao.findById(request.getAppService()).isPresent())
+			throw new NoDataException("Implementation  not present");
+		si.setAppService(appServiceDao.findById(request.getAppService()).get());
+
+		if (!statisticalMethodDao.findById(request.getStatMethod()).isPresent())
+			throw new NoDataException("Statistical method  not present");
+		si.setStatMethod(statisticalMethodDao.findById(request.getStatMethod()).get());
+
+		stepInstanceDao.save(si);
+
 		return Translators.translate(si);
 	}
-	public StepInstanceDto deleteStepInstance(Integer id) {		
+
+	public StepInstanceDto deleteStepInstance(Integer id) {
 		if (!stepInstanceDao.findById(id).isPresent())
 			throw new NoDataException("StepInstance not present");
-			StepInstance si = stepInstanceDao.findById(id).get();
-			stepInstanceDao.delete(si);
-			return Translators.translate(si);		
+		StepInstance si = stepInstanceDao.findById(id).get();
+		stepInstanceDao.delete(si);
+		return Translators.translate(si);
 	}
 
 	public List<StepInstanceInverseDto> findStepInstancesByAppService(Integer id) {
-		
+
 		return Translators.translateStepInstancesInverse(stepInstanceDao.findByAppService(new AppService(id)));
 	}
 }
-
