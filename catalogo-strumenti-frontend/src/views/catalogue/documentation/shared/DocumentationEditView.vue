bc138de75e6b01c954c31197e6642438754066d2
SMacOne <s.macone@libero.it>
2022-05-12 10:08:21 +0200

    fix

--- catalogo-strumenti-frontend/src/views/catalogue/documentation/shared/DocumentationEditView.vue
+++ catalogo-strumenti-frontend/src/views/catalogue/documentation/shared/DocumentationEditView.vue
@@ -1,70 +1,69 @@
 <template>
   <div>
-    <CCardHeader>{{ this.toolName | dashEmpty }} > Documentazione
-     <div v-if="!viewNewDocument" class="card-header-actions">             <span
-          class="icon-link float-right"
-          @click="viewNewDocument = true"
+    <CCardHeader
+      >{{ this.toolName | dashEmpty }} > Documentazione
+      <div v-if="!viewNewDocument" class="card-header-actions">
+        <span class="icon-link float-right" @click="viewNewDocument = true"
           ><add-icon title="Aggiungi un nuovo documento" />&nbsp;Nuovo
           Documento</span
         >
-            </div>  
-              </CCardHeader>
- 
-    
-      <div v-if="viewNewDocument" class="col-12">
-        <CCard class="col-12">
-          <CCardHeader>Nuovo Documento </CCardHeader>
-          <CCardBody>
-            <CInput
-              label="Nome"
-              placeholder="Nome"
-              v-model="documentationLocal.name"
-            />
-            <CInput
-              label="Editore"
-              placeholder="Editore"
-              v-model="documentationLocal.publisher"
-            />
-            <div>
-              <label>Tipo Documento</label>
-            </div>
-            <v-select
-              label="name"
-              :options="documentationTypeList"
-              placeholder="Tipo documento"
-              v-model="documentationLocal.documentType"
-            ></v-select>
-            <CTextarea
-              label="Note"
-              placeholder="Note"
-              v-model="documentationLocal.notes"
-            />
-            <CInput
-              label="Fonti"
-              placeholder="Fonti"
-              v-model="documentationLocal.resource"
-            />
-          </CCardBody>
-          <CCardFooter>
-            <CButton
-              shape="square"
-              size="sm"
-              color="primary"
-              class="mr-2"
-              @click.prevent="handleSubmit"
-              >Salva</CButton
-            >
-            <CButton
-              shape="square"
-              size="sm"
-              color="light"
-              @click.prevent="viewNewDocument = false"
-              >Chiudi</CButton
-            >
-          </CCardFooter>
-        </CCard>
       </div>
-  <CCardBody>
+    </CCardHeader>
+
+    <div v-if="viewNewDocument" class="col-12">
+      <CCard class="col-12">
+        <CCardHeader>Nuovo Documento </CCardHeader>
+        <CCardBody>
+          <CInput
+            label="Nome"
+            placeholder="Nome"
+            v-model="documentationLocal.name"
+          />
+          <CInput
+            label="Editore"
+            placeholder="Editore"
+            v-model="documentationLocal.publisher"
+          />
+          <div>
+            <label>Tipo Documento</label>
+          </div>
+          <v-select
+            label="name"
+            :options="documentationTypeList"
+            placeholder="Tipo documento"
+            v-model="documentationLocal.documentType"
+          ></v-select>
+          <CTextarea
+            label="Note"
+            placeholder="Note"
+            v-model="documentationLocal.notes"
+          />
+          <CInput
+            label="Fonti"
+            placeholder="Fonti"
+            v-model="documentationLocal.resource"
+          />
+        </CCardBody>
+        <CCardFooter>
+          <CButton
+            shape="square"
+            size="sm"
+            color="primary"
+            class="mr-2"
+            @click.prevent="handleSubmit"
+            >Salva</CButton
+          >
+          <CButton
+            shape="square"
+            size="sm"
+            color="light"
+            @click.prevent="viewNewDocument = false"
+            >Chiudi</CButton
+          >
+        </CCardFooter>
+      </CCard>
+    </div>
+    <CCardBody>
       <div class="row">
         <div v-if="documentations.length === 0">
           <span>Nessuna documentazione associata</span>
@@ -72,39 +71,37 @@
         <div v-else>
           <p class="card-text">Elenco documenti disponibili:</p>
         </div>
-     
       </div>
-   
 
-    <div class="row">
-      <div
-        class="card col-3"
-        v-for="documentation of documentations"
-        :key="documentation.id"
-      >
-        <div class="card-header">
-          {{ documentation.name }}
-          <div class="card-header-actions">
-            <router-link
-              tag="a"
-              :to="{
-                name: 'DocumentationDetails',
-                params: { id: documentation.id },
-              }"
-            >
-              <view-icon />
-            </router-link>
-            <span class="icon-link" @click="modalOpen(documentation)"
-              ><delete-icon />
-            </span>
+      <div class="row">
+        <div
+          class="card col-3"
+          v-for="documentation of documentations"
+          :key="documentation.id"
+        >
+          <div class="card-header">
+            {{ documentation.name }}
+            <div class="card-header-actions">
+              <router-link
+                tag="a"
+                :to="{
+                  name: 'DocumentationDetails',
+                  params: { id: documentation.id }
+                }"
+              >
+                <view-icon />
+              </router-link>
+              <span class="icon-link" @click="modalOpen(documentation)"
+                ><delete-icon />
+              </span>
+            </div>
+          </div>
+          <div class="card-body">
+            <p class="card-text">{{ documentation.documentType }}</p>
           </div>
         </div>
-        <div class="card-body">
-          <p class="card-text">{{ documentation.documentType }}</p>
-        </div>
       </div>
-    </div>
-   </CCardBody>
+    </CCardBody>
     <CModal
       title="Warning!"
       :show.sync="warningModal"
@@ -146,12 +143,12 @@ export default {
         publisher: "",
         documentType: "",
         resource: "",
-        tool: this.toolId,
-      },
+        tool: this.toolId
+      }
     };
   },
   computed: {
-    ...mapGetters("documentationType", ["documentationTypeList"]),
+    ...mapGetters("documentationType", ["documentationTypeList"])
   },
   emits: ["refreshTool"],
 
@@ -159,18 +156,18 @@ export default {
     documentations: {
       type: Array,
       required: true,
-      default: () => [],
+      default: () => []
     },
     toolId: {
       type: Number,
       required: true,
-      default: null,
+      default: null
     },
-     toolName: {
+    toolName: {
       type: String,
       required: true,
-      default: null,
-    },
+      default: null
+    }
   },
   methods: {
     changeTool(value) {
@@ -181,8 +178,7 @@ export default {
     },
     handleSubmit() {
       this.documentationLocal.tool = this.toolId;
-      this.documentationLocal.documentType =
-        this.documentationLocal.documentType.id;
+      this.documentationLocal.documentType = this.documentationLocal.documentType.id;
       console.log(this.documentationLocal);
       this.$store
         .dispatch("documentation/save", this.documentationLocal)
@@ -205,13 +201,13 @@ export default {
     },
     modalClose() {
       this.warningModal = false;
-    },
+    }
   },
   created() {
     //this.$store.dispatch("documentation/findAll");
     //this.$store.dispatch("tools/findAll");
     this.$store.dispatch("documentationType/findAll");
-  },
+  }
 };
 </script>
 <style scoped>
