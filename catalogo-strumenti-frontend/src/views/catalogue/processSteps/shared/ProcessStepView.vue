b2a4403e75e3f54bff1ea162e3b5cfbe3e509fa4
SMacOne <s.macone@libero.it>
2022-05-16 17:07:19 +0200

    fix

--- catalogo-strumenti-frontend/src/views/catalogue/processSteps/shared/ProcessStepView.vue
+++ catalogo-strumenti-frontend/src/views/catalogue/processSteps/shared/ProcessStepView.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <h2><span>2</span> Passi del processo: {{processName}}</h2>
+    <h2><span>2</span> Passi del processo: {{ processName }}</h2>
     <div v-if="processSteps">
-      <div
-        v-for="(processStep, index) of processSteps"
-        :key="processStep.id"
-      >
+      <div v-for="(processStep, index) of processSteps" :key="processStep.id">
         <div class="p-2">
-          <h4 ><span>2.{{index + 1}}</span> {{ processStep.name }}</h4>
+          <h4>
+            <span>2.{{ index + 1 }}</span> {{ processStep.name }}
+          </h4>
           <div class="columns">
             <div class="row">
               <div class="description-fields col-12">
@@ -19,89 +18,81 @@
                   <span v-if="processStep">{{ processStep.label }}</span>
                 </div>
               </div>
-      
             </div>
           </div>
-    <div class="card-body">
-      
-              <!--div class="card-body"-->
-          <!-- @start Condition to show filtrable table if results are more then 5 lines-->
-          <div
-            class="table-responsive"
-            v-if="processStep.processDesigns.length > 20"
-          >
-            <CDataTable
-              :items="getProcessDesignes(processStep.processDesigns)"
-              :fields="fields"
-              column-filter
-              table-filter
-              items-per-page-select
-              :items-per-page="5"
-              hover
-              sorter
-              pagination
+          <div class="card-body">
+            <!--div class="card-body"-->
+            <!-- @start Condition to show filtrable table if results are more then 5 lines-->
+            <div
+              class="table-responsive"
+              v-if="processStep.processDesigns.length > 20"
             >
-            </CDataTable>
-          </div>
-          <!-- @end Condition to show filtrable table if results are more then 5 lines-->
-          <table
-            class="table table-hover"
-            v-if="appService && processStep.processDesigns.length < 20"
-          >
-            <thead>
-              <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Tipo</th>
-                <th scope="col">ioNome</th>
-                <th scope="col">ioDescr</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="item in getProcessDesignes(processStep.processDesigns)"
-                :key="item.id"
+              <CDataTable
+                :items="getProcessDesignes(processStep.processDesigns)"
+                :fields="fields"
+                column-filter
+                table-filter
+                items-per-page-select
+                :items-per-page="5"
+                hover
+                sorter
+                pagination
               >
-                <td>{{ item.name }}</td>
-                <td>{{ item.descr }}</td>
-                <td>{{ item.type }}</td>
-                <td>{{ item.informationObjectName }}</td>
-                 <td>{{ item.informationObjectDescr }}</td>
-  
-
-  
-  </tr>
-            </tbody>
-          </table>
-          <h5
-            v-if="!processStep.processDesigns.length"
-            class="default-value card-body"
-          >
-            Nessun dato disponibile
-          </h5>
-    </div>
-         
+              </CDataTable>
+            </div>
+            <!-- @end Condition to show filtrable table if results are more then 5 lines-->
+            <table
+              class="table table-hover"
+              v-if="appService && processStep.processDesigns.length < 20"
+            >
+              <thead>
+                <tr>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Descrizione</th>
+                  <th scope="col">Tipo</th>
+                  <th scope="col">ioNome</th>
+                  <th scope="col">ioDescr</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in getProcessDesignes(processStep.processDesigns)"
+                  :key="item.id"
+                >
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.descr }}</td>
+                  <td>{{ item.type }}</td>
+                  <td>{{ item.informationObjectName }}</td>
+                  <td>{{ item.informationObjectDescr }}</td>
+                </tr>
+              </tbody>
+            </table>
+            <h5
+              v-if="!processStep.processDesigns.length"
+              class="default-value card-body"
+            >
+              Nessun dato disponibile
+            </h5>
+          </div>
         </div>
         <!--/div-->
         <!--/fieldset-->
       </div>
     </div>
- 
   </div>
 
   <!--/div>
   </div-->
 </template>
 <script>
-
 export default {
   name: "ProcessStepView",
-  components: {  },
-    data() {
+  components: {},
+  data() {
     return {
       disabled: false,
       fields: [
-         {
+        {
           key: "id",
           label: "id"
         },
@@ -128,20 +119,20 @@ export default {
       ]
     };
   },
-    methods: {
+  methods: {
     getProcessDesignes: function(processDesignes) {
-     if(processDesignes) 
-      return processDesignes.map(pDesign => {
-        return {
-          id: pDesign.id,
-          name: pDesign.name,
-          descr: pDesign.descr,
-          type: pDesign.designType.type,
-          informationObjectName: pDesign.informationObject.name,
-          informationObjectDescr: pDesign.informationObject.descr
-        };
-      });
-      else return[];
+      if (processDesignes)
+        return processDesignes.map(pDesign => {
+          return {
+            id: pDesign.id,
+            name: pDesign.name,
+            descr: pDesign.descr,
+            type: pDesign.designType.type,
+            informationObjectName: pDesign.informationObject.name,
+            informationObjectDescr: pDesign.informationObject.descr
+          };
+        });
+      else return [];
     }
   },
   props: {
@@ -153,9 +144,9 @@ export default {
     processName: {
       type: String,
       required: true,
-      default: () => ''
+      default: () => ""
     },
-     processIndex: {
+    processIndex: {
       type: Number,
       required: true,
       default: () => 1
