81772d277af3f97c755b7587057d1ffee74b8b3f
papizzo <papizzo@istat.it>
2022-12-07 08:41:58 +0100

    fix process design edit new

--- catalogo-strumenti-frontend/src/components/businessProcess/CBusinessProcessDesignNew.vue
+++ catalogo-strumenti-frontend/src/components/businessProcess/CBusinessProcessDesignNew.vue
@@ -9,106 +9,131 @@
       @handleSubmit="handleSubmit"
       @handleBack="handleBack"
     />
+    <!--CCard>
+      <CCardBody-->
+    <Label>Process Design</Label>
     <CCard>
       <CCardBody>
-        <div class="row mt-4">
-          <label class="col-12 pl-2">Process Design</label>
-          <div class="row">
-            <CInput
-              class="col-6"
-              label="index"
-              placeholder="index"
-              v-model="bProcessDesignLocal.processDesigns_index"
-            />
-            <CInput
-              class="col-6"
-              label="id"
-              placeholder="id"
-              v-model="bProcessDesignLocal.processDesigns_id"
-            />
-            <CTextarea
-              class="col-12"
-              label="description"
-              placeholder="processDesigns_descr"
-              v-model="bProcessDesignLocal.processDesigns_descr"
-            />
-          </div>
+        <div class="row">
+          <CInput
+            class="col-2"
+            label="index"
+            placeholder="index"
+            v-model="bProcessDesignLocal.processDesigns_index"
+          />
+          <CInput
+            class="col-2"
+            label="id"
+            placeholder="id"
+            v-model="bProcessDesignLocal.processDesigns_id"
+          />
+          <CTextarea
+            class="col-8"
+            label="description"
+            placeholder="processDesigns_descr"
+            v-model="bProcessDesignLocal.processDesigns_descr"
+          />
         </div>
-
-        <div class="row mt-4">
-          <label class="col-12 pl-2">Process Design Description</label>
-          <div class="row">
-            <CInput
-              class="col-6"
-              label="id"
-              placeholder="id"
-              v-model="bProcessDesignLocal.processDesignDescription_id"
-            />
-            <CTextarea
-              class="col-12"
-              label="description"
-              placeholder="description"
-              v-model="bProcessDesignLocal.processDesignDescription_description"
-            />
-          </div>
+      </CCardBody>
+    </CCard>
+    <Label>Process Design Description</Label>
+    <CCard>
+      <CCardBody>
+        <div class="row">
+          <CInput
+            class="col-2"
+            label="id"
+            placeholder="id"
+            v-model="bProcessDesignLocal.processDesignDescription_id"
+          />
+          <CTextarea
+            class="col-10"
+            label="description"
+            placeholder="description"
+            v-model="bProcessDesignLocal.processDesignDescription_description"
+          />
         </div>
+      </CCardBody>
+    </CCard>
+    <Label>Design Type</Label>
+    <CCard>
+      <CCardBody>
+        <div class="row">
+          <CInput
+            class="col-2"
+            label="Index"
+            placeholder="index"
+            v-model="bProcessDesignLocal.index"
+          />
+          <CInput
+            class="col-2"
+            label="id"
+            placeholder="id"
+            v-model="bProcessDesignLocal.designType_id"
+          />
+          <!--CCInput
+            class="col-8"
+            label="type"
+            placeholder="type"
+            v-model="bProcessDesignLocal.designType_type"
+          /-->
 
-        <div class="row mt-4">
-          <label class="col-12 pl-2">Design Type</label>
-          <div class="row">
-            <CInput
-              class="col-4"
-              label="Index"
-              placeholder="index"
-              v-model="bProcessDesignLocal.index"
-            />
-            <CInput
-              class="col-4"
-              label="id"
-              placeholder="id"
-              v-model="bProcessDesignLocal.designType_id"
-            />
-            <CInput
-              class="col-4"
-              label="designType_type"
-              placeholder="designType_type"
+          <div class="form-group col-4" role="group">
+            <label class="col-12">type</label>
+            <v-select
+              label="type"
+              class="col-12 p-0"
+              :options="bProcessDesignLocal.designType_type"
+              placeholder="type"
               v-model="bProcessDesignLocal.designType_type"
-            />
+            ></v-select>
           </div>
-        </div>
-
-        <div class="row mt-4">
-          <label class="col-12 pl-2">information Object</label>
-          <div class="row">
-            <CInput
-              class="col-3"
-              label="csmAppRoleId"
-              placeholder="csmAppRoleId"
-              v-model="bProcessDesignLocal.informationObject_csmAppRoleId"
-            />
-            <CInput
-              class="col-2"
-              label="id"
-              placeholder="id"
-              v-model="bProcessDesignLocal.informationObject_id"
-            />
-            <CInput
-              class="col-7"
-              label="name"
-              placeholder="name"
-              v-model="bProcessDesignLocal.informationObject_name"
-            />
-
-            <CTextarea
-              class="col-12 mt-2"
-              label="description"
-              placeholder="description"
-              v-model="bProcessDesignLocal.informationObject_description"
-            />
+          <div class="form-group col-4" role="group">
+            <label class="col-12">subtype</label>
+            <v-select
+              label="type"
+              class="col-12 p-0"
+              :options="bProcessDesignLocal.designType_type"
+              placeholder="type"
+              v-model="bProcessDesignLocal.designType_type"
+            ></v-select>
           </div>
         </div>
       </CCardBody>
     </CCard>
+    <Label>information Object</Label>
+    <CCard>
+      <CCardBody>
+        <div class="row">
+          <CInput
+            class="col-2"
+            label="csmAppRoleId"
+            placeholder="csmAppRoleId"
+            v-model="bProcessDesignLocal.informationObject_csmAppRoleId"
+          />
+          <CInput
+            class="col-2"
+            label="id"
+            placeholder="id"
+            v-model="bProcessDesignLocal.informationObject_id"
+          />
+          <CInput
+            class="col-8"
+            label="name"
+            placeholder="name"
+            v-model="bProcessDesignLocal.informationObject_name"
+          />
+          <CTextarea
+            class="col-12"
+            label="description"
+            placeholder="description"
+            v-model="bProcessDesignLocal.informationObject_description"
+          />
+        </div>
+      </CCardBody>
+    </CCard>
+    <!--/CCardBody>
+    </CCard-->
   </div>
 </template>
 <script>
