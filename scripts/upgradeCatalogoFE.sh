dc94b9aa00ca97ca382086b31b6674b25c54c51b
Francesco Amato <49312682+francescoamato@users.noreply.github.com>
2023-03-15 11:35:38 +0100

    Update upgradeCatalogoFE.sh

--- scripts/upgradeCatalogoFE.sh
+++ scripts/upgradeCatalogoFE.sh
@@ -1,7 +1,7 @@
 cd /home/is2admin/git/catalogo-strumenti/
 git reset --hard
 git pull
-kill -9 $(lsof -t -i:8071)
+kill -9 $(lsof -t -i:9071)
 cd /home/is2admin/git/catalogo-strumenti/catalogo-strumenti-frontend
 npm install
 nohup npm run serve&  
